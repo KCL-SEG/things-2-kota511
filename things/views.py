90bab768839bbc4a3bef01b5af50db891b5d25d3
Kota Amemiya <kotaamemiya@gmail.com>
2023-12-07 05:49:02 +0000

    First commit

--- things/views.py
+++ things/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thing added successfully!')
+        else:
+            messages.error(request, 'Error adding thing.')
+    else:
+        form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
+        
